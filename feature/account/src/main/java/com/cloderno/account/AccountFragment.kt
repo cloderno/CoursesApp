aa9fdf64dae6f17bad85cf897ffed184b33063f4
cloderno <cloderno@gmail.com>
2026-05-27 22:00:44 +0500

    account fragment

---
+++ feature/account/src/main/java/com/cloderno/account/AccountFragment.kt
@@ -0,0 +1,17 @@
+package com.cloderno.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yeldar.account.R
+
+
+class AccountFragment : Fragment(R.layout.fragment_account) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
